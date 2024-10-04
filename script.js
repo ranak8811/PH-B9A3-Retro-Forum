0182d000d8dff7bcc814dda7778e6e9a71aa9e69
Anwar_Hossain <ranakrphone@gmail.com>
2024-10-04 18:51:24 +0600

    all parts completed

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const loadAllPosts = async (category) => {
 
   // console.log(category);
 
+  document.getElementById("post-container").innerHTML = "";
+
   const response = await fetch(
     `https://openapi.programming-hero.com/api/retro-forum/posts${
       category ? `?category=${category}` : ""
     }`
   );
   const data = await response.json();
+  // console.log(data);
   displayAllPosts(data.posts);
 };
 
@@ -127,3 +130,66 @@ const handleSearchByCategory = () => {
   const searchText = document.getElementById("searchPosts").value;
   loadAllPosts(searchText);
 };
+
+const loadLatestPosts = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
+  );
+  const data = await res.json();
+  // console.log(data);
+  displayLatestPosts(data);
+};
+
+const displayLatestPosts = (posts) => {
+  // console.log(posts);
+
+  const latestPostContainer = document.getElementById("latest-post-container");
+
+  posts.forEach((post) => {
+    // console.log(post);
+
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+               <div class="card lg:w-96 pb-5 bg-base-100 shadow-2xl">
+             <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+               <img
+                 src="${post.cover_image}"
+                 alt="Post Cover Image"
+                 class="rounded-xl"
+               />
+             </figure>
+             <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+               <p class="opacity-50 text-start">
+                 <i class="fa-solid fa-calendar-days me-2"></i>
+                 ${post.author?.posted_date || "No Publish Date"}
+               </p>
+               <h2 class="card-title text-start">${post.title}</h2>
+               <p class="text-start">${post.description}</p>
+               <div class="card-actions flex gap-5 items-center">
+                 <div class="avatar">
+                   <div
+                     class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+                   >
+                     <img src="${post.profile_image}" alt="Author Image" />
+                   </div>
+                 </div>
+                 <div>
+                   <h3 class="text-start font-extrabold">${
+                     post.author.name
+                   }</h3>
+                   <p class="text-start opacity-60">
+                     ${post.author?.designation || "Unknown"}
+                   </p>
+                 </div>
+               </div>
+             </div>
+           </div>
+  
+   `;
+
+    latestPostContainer.appendChild(div);
+  });
+};
+
+loadLatestPosts();
